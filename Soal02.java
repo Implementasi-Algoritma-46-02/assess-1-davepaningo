e57df61cb20a8d134e94652264af65aecc6830b2
davepaningo <davepaningo2@gmail.com>
2024-10-29 16:29:19 +0700

    tes

--- Soal02.java
+++ Soal02.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
+
 public class Soal02 {
 
 	public static void main(final String[] args) {
 		// Kerjakan soalnya di sini
-	}
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("");
+		double waktu = scanner.nextDouble();
+		System.out.print("");
+		double ipk = scanner.nextDouble();
+		System.out.print("");
+		int eprt = scanner.nextInt();
+		System.out.print("");
+		String hak = scanner.nextLine();
+		System.out.print("");
+		String lomba = scanner.nextLine();
+
+		boolean cumlaude;
+
+		if (hak.equals("ada")) {
+			System.out.println("Cumlaude");;
+			} else if (lomba.equals("ada")) {
+			System.out.println("Cumlaude");;
+		    } else if (waktu <= 3.0) {
+				System.out.println("Cumlaude");;
+			} else if (ipk >= 3.5) {
+				System.out.println("Cumlaude");;
+			} else if (eprt >= 450) {
+				System.out.println("Cumlaude");;
+			} else {
+				System.out.println("Tidak Cumlaude");
+				return;
+			}
 }
+}
+
--- Soal03.java
+++ Soal03.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 public class Soal03 {
 
 	public static void main(final String[] args) {
 		// Kerjakan soalnya di sini
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("");
+		int H = scanner.nextInt();
+		System.out.print("");
+		int M = scanner.nextInt();
+		System.out.print("");
+		int V = scanner.nextInt();
+		
+
+		int hasil = M + V;
+		System.out.println(H + ":" + hasil);
+
+
+
+
 	}
 }
